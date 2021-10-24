ee1bda9721f5d733f6cd2a6a553228efb40e0d21
pi2411 <68503627+pi2411@users.noreply.github.com>
2021-10-24 15:57:25 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require('passport-local-mongoose');
 const findOrCreate = require('mongoose-findorcreate');
-mongoose.connect("mongodb+srv://admin_ygal:test123@cluster0.es7mi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://process.env.CLINET_ADMIN:process.env.CLINET_ADMIN_SECRET@cluster0.es7mi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
